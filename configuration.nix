b169cd0423cfb535d4ae00cc9840111b6b242281
Max Edwards <youwontforgetthis@gmail.com>
2026-04-02 20:21:57 +0000

    initial move to flakes

--- configuration.nix
+++ configuration.nix
@@ -1,21 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-# NixOS-WSL specific options are documented on the NixOS-WSL repository:
-# https://github.com/nix-community/NixOS-WSL
-
 { config, lib, pkgs, ... }:
-
 {
-  imports = [
-    # include NixOS-WSL modules
-    <nixos-wsl/modules>
-  ];
-
   wsl.enable = true;
   wsl.defaultUser = "max";
-  # System packages
+
   environment.systemPackages = with pkgs; [
     wget
     curl
@@ -23,7 +10,6 @@
     neovim
   ];
 
-  # SSH daemon
   services.openssh = {
     enable = true;
     ports = [ 4444 ];
@@ -33,7 +19,6 @@
     };
   };
 
-  # Add your Mac's public key
   users.users.max = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
@@ -45,19 +30,15 @@
   programs.git = {
     enable = true;
     config = {
-      init = { defaultBranch = "main"; };
+      init.defaultBranch = "main";
       user.name = "Max Edwards";
       user.email = "youwontforgetthis@gmail.com";
     };
   };
 
-  # Passwordless sudo (optional but convenient for WSL)
   security.sudo.wheelNeedsPassword = false;
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "25.11";
 }
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Max's NixOS WSL configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-wsl.nixosModules.default
+        ./configuration.nix
+      ];
+    };
+  };
+}
